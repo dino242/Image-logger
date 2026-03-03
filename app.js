323b310d45fb132b25fa652348a6d3a232f1f368
Dino <webhook36@gmail.com>
2026-03-03 23:22:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-const GIF_URL = "https://i.kym-cdn.com/photos/images/original/003/153/306/f1f.gif";
+const GIF_URL = "https://raw.githubusercontent.com/dino242/Image-logger/refs/heads/main/f1f.gif";
 
 app.get('/image.gif', (req, res) => {
-    const info = {
-        ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
-        time: new Date().toISOString(),
-        ua: req.headers['user-agent']
-    };
-
-    console.log("Neuer Zugriff:", info);
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    
+    console.log("NEUER LOG!");
+    console.log("IP:", ip);
+    console.log("UA:", req.headers['user-agent']);
 
     res.redirect(GIF_URL);
 });
 
+app.get('/', (req, res) => {
+    res.send("Server läuft, Cheffe!");
+});
+
 app.listen(PORT, () => {
-    console.log(`Server läuft auf Port ${PORT}`);
+    console.log("Server ist bereit.");
 });
